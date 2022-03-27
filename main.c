a29203a1a3d08d162114ee73cdcdc18618909130
beijitu1 <1448586320@qq.com>
2022-03-27 21:18:34 +0800

    创建mian.c

---
+++ main.c
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: main.c
+	> Author: tc
+	> Mail: 666666@qq.com 
+	> Created Time: 2022年03月27日 星期日 21时17分32秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<pthread.h>
+int main()
+{
+	return ;
+}
